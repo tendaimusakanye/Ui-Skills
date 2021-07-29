141049e5ba203e16a29d284add879b32b11c4c4d
Tendai <59752146+tendaimusakanye@users.noreply.github.com>
2021-07-29 18:33:49 +0200

    Initial commit

---
+++ app/src/main/java/k7tech/agency/uiskills/MainActivity.kt
@@ -0,0 +1,11 @@
+package k7tech.agency.uiskills
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
