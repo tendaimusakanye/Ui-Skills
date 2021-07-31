acafe6206cb63d47ab45ffe49afa64645276a484
Tendai <59752146+tendaimusakanye@users.noreply.github.com>
2021-07-31 20:26:04 +0200

    Remove unused import

--- app/src/main/java/k7tech/agency/uiskills/feed/FeedFragment.kt
+++ app/src/main/java/k7tech/agency/uiskills/feed/FeedFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
